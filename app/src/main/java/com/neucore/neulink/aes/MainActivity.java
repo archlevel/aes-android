06a04a58dad7040b963634cf4003980fc53504a1
jeff <arch_level@126.com>
2020-08-27 20:57:40 +0800

    优化代码

--- app/src/main/java/com/neucore/neulink/aes/MainActivity.java
+++ app/src/main/java/com/neucore/neulink/aes/MainActivity.java
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
 
+import java.util.Properties;
+
 public class MainActivity extends AppCompatActivity {
 
     // Used to load the 'native-lib' library on application startup.
@@ -19,9 +21,27 @@ public class MainActivity extends AppCompatActivity {
 
         // Example of a call to a native method
         TextView tv = findViewById(R.id.sample_text);
-        String enc = AesUtil.encrypt("java");
-        tv.setText(AesUtil.decrypt(enc));
-        //tv.setText(AesUtil.decrypt("java"));
+
+        String plain = "FTP.Server=ftp://10.18.105.254:21;\n" +
+                "Topic.Partition=ftproot;\n" +
+                "FTP.UserName=neu2ftp;\n" +
+                "MQTT-Server=tcp://mqtt.neucore.com:1883;\n" +
+                "FTP.Password=123456;\n" +
+                "Storage.Type=OSS;\n" +
+                "OSS.AccessKeySecret=fJPkLrpdMRIEB6BxdwbpD1xJsivC9h;\n" +
+                "OSS.AccessKeyID=LTAI4G6ZyiqBdrY9HA1Np6To;\n" +
+                "OSS.BucketName=neudevice;\n" +
+                "OSS.EndPoint=https://oss-cn-shanghai.aliyuncs.com;\n" +
+                "FTP.BucketName=ftproot;\n" +
+                "Log.Level=W;\n";
+        plain = "java1111111111111212121212123232323212121212121212121java1111111111111212121212123232323212121212121212121java1111111111111212121212123232323212121212121212121";
+        System.out.println(plain);
+        String enc = AesUtil.encrypt(plain);
+        System.out.println(enc);
+        String plain_ = AesUtil.decrypt(enc);
+        System.out.println(plain_);
+
+        tv.setText(plain_);
     }
 
     /**
